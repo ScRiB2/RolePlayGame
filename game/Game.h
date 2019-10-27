939d6bf6c3e262323574ca2a31446bd57d6de912
ScRiB <scrib2@yandex.ru>
2019-10-27 21:01:43 +0300

    In game

--- game/Game.h
+++ game/Game.h
@@ -11,13 +11,46 @@
 #include "../item/weapons/shotgun/Shotgun.h"
 
 class Game {
-//    Player one = Player("Петя");
-//    Player two = Player("Вася");
+    vector<Weapon> weapons;
 
 public:
     void start(){
-//        Pistol::Pistol GLock = new Pistol::Pistol();
+        Player one = Player("Петя");
+        Player two = Player("Вася");
 
+        Rifle AK = Rifle("AK-47", 36, 2700, 30, 40); //2700
+        Shotgun SWAG = Shotgun("SWAG-7", 50, 1800, 10, 30);  //1800
+
+        weapons.push_back(AK);
+        weapons.push_back(SWAG);
+
+        one.buyWeapon(SWAG).upgradeWeapon(AIM);
+        one.buyArmor();
+
+        one.action(&two);
+
+        for (int i = 0; i < 5; i++) {
+            while (!two.isDie() && !one.isDie()) {
+                one.action(&two);
+                two.action(&one);
+            }
+            if (two.isDie()) {
+                two.respawn();
+                two.buyArmor();
+            } else {
+                one.respawn();
+                one.buyArmor();
+                one.buyWeapon(SWAG);
+            }
+        }
+
+        cout << one.getName() + ": " << one.getKilled() << std::endl;
+        cout << two.getName() + ": " << two.getKilled();
+
+    }
+
+    vector<Weapon> getWeapons(){
+        return weapons;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -12,36 +12,9 @@ using namespace std;
 int main() {
     system("chcp 65001");
 
-    Player one = Player("Петя");
-    Player two = Player("Вася");
 
-    Rifle AK = Rifle("AK-47", 36, 2700, 30, 40); //2700
-    Shotgun SWAG = Shotgun("SWAG-7", 50, 1800, 10, 30);  //1800
-
-    one.buyWeapon(SWAG).upgradeWeapon(AIM);
-    one.buyArmor();
-
-    one.action(&two);
-
-    for (int i = 0; i < 5; i++) {
-        while (!two.isDie() && !one.isDie()) {
-            one.action(&two);
-            two.action(&one);
-        }
-        if (two.isDie()) {
-            two.respawn();
-            two.buyArmor();
-        } else {
-            one.respawn();
-            one.buyArmor();
-            one.buyWeapon(SWAG);
-        }
-    }
-
-    cout << one.getName() + ": " << one.getKilled() << std::endl;
-    cout << two.getName() + ": " << two.getKilled();
-//    Game game;
-//    game.start();
+    Game game;
+    game.start();
 
     return 0;
 }
